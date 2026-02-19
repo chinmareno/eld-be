7263986d64ccd8eaa7bd00130cef2f56246e9cc5
chinmareno <chinmareno1@gmail.com>
2026-02-19 14:22:10 +0700

    feat: return all user fields except password

--- project/api/serializer.py
+++ project/api/serializer.py
@@ -7,7 +7,7 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ("id", "username", "email")
+        exclude = ("password",)
 
 
 class LoginSerializer(serializers.Serializer):
